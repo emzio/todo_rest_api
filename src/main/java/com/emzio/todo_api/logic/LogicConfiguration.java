ab2ac5db5a3391fcbe752e26edf19a4e5f126253
emzio <emziolkow@gmail.com>
2023-01-31 11:46:39 +0100

    added presentation ProjectService XMLconfig

--- src/main/java/com/emzio/todo_api/logic/LogicConfiguration.java
+++ src/main/java/com/emzio/todo_api/logic/LogicConfiguration.java
@@ -5,16 +5,18 @@ import com.emzio.todo_api.model.ProjectRepository;
 import com.emzio.todo_api.model.TaskGroupsRepository;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.ImportResource;
 
 @Configuration
+@ImportResource("classpath:applicationContext.xml")
 public class LogicConfiguration {
 
-    @Bean
+    /*@Bean
     ProjectService projectService(
             ProjectRepository repository,
             TaskGroupsRepository taskGroupsRepository,
             TaskConfigurationProperties configuration
     ){
         return new ProjectService(repository, taskGroupsRepository, configuration);
-    }
+    }*/
 }
