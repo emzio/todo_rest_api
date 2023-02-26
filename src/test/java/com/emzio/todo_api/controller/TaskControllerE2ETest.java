d1a97c7d185a1d47a1dd1f683395b003ff0b64ce
emzio <emziolkow@gmail.com>
2023-02-26 14:20:53 +0100

    finisched tests

--- src/test/java/com/emzio/todo_api/controller/TaskControllerE2ETest.java
+++ src/test/java/com/emzio/todo_api/controller/TaskControllerE2ETest.java
@@ -9,24 +9,25 @@ import org.json.JSONException;
 import org.json.JSONObject;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.boot.test.web.server.LocalServerPort;
-import org.springframework.http.HttpEntity;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.MediaType;
-import org.springframework.http.RequestEntity;
+import org.springframework.http.*;
 import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.web.client.RestTemplate;
 
-import java.net.http.HttpRequest;
 import java.time.LocalDateTime;
-import java.util.Objects;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+// TODO delete profile integrationTmp
 @ActiveProfiles("integrationTmp")
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+//@AutoConfigureMockMvc
 class TaskControllerE2ETest {
 
     private final ObjectMapper objectMapper = new ObjectMapper();
@@ -35,6 +36,9 @@ class TaskControllerE2ETest {
 
     @Autowired
     private TestRestTemplate restTemplate;
+// TODO put check status.ok
+//    @Autowired
+//    private MockMvc mockMvc;
 
     @Autowired
     TaskRepository repo;
@@ -64,46 +68,111 @@ class TaskControllerE2ETest {
 
     @Test
     void httpPost_returnsSavedTask(){
+        //TODO JsonNode method
+//        //given
+//        String url = "http://localhost:" + port + "/tasks";
+//        //and
+//        var taskJsonObject = getJsonObject(false, "JsonTest", LocalDateTime.now());
+//        // and
+//        HttpEntity<String> httpEntity = getHttpEntity(taskJsonObject);
+//        // when
+//        String result = restTemplate.postForObject(url, httpEntity, String.class);
+//        try {
+//            JsonNode root = objectMapper.readTree(result);
+//            //        assertThat(result).isInstanceOf(Task.class);
+////            assertThat(root).flatExtracting("description").isEqualTo("JsonTest");
+//            assertThat(result).contains("description");
+//            assertThat(result).contains("JsonTest");
+//        } catch (JsonProcessingException e) {
+//            throw new RuntimeException(e);
+//        }
         //given
-        int id = repo.save(new Task("foo", LocalDateTime.now())).getId();
-//        String reqJson =
-//                "    \"done\": true," +
-//                "    \"description\": \"test json\"," +
-//                "    \"deadline\": \"2023-10-12T23:59:59.999\"";
-        // and
         String url = "http://localhost:" + port + "/tasks";
         //and
-        var headers = new HttpHeaders();
-        headers.setContentType(MediaType.APPLICATION_JSON);
+        var taskJsonObject = getJsonObject(false, "JsonTest", LocalDateTime.now());
         // and
-        var restTemplate = new RestTemplate();
+        HttpEntity<String> httpEntity = getHttpEntity(taskJsonObject);
+        // when
+        Task result = restTemplate.postForObject(url, httpEntity, Task.class);
+        // then
+        assertThat(result).isInstanceOf(Task.class);
+        assertThat(result.getDescription()).isEqualTo("JsonTest");
+    }
+
+    @Test
+    void httpPut_returnsNoContent(){
+        //given
+        int id = repo.save(new Task("foo", LocalDateTime.now())).getId();
         // and
+        String url = "http://localhost:" + port + "/tasks/" + id;
+        //and
+        JSONObject taskJsonObject = getJsonObject(true, "Put", LocalDateTime.now());
+        HttpEntity<String> httpEntity = getHttpEntity(taskJsonObject);
+        //when
+        restTemplate.put(url,httpEntity);
+        Task result = repo.findById(id).orElse(null);
+        //then
+        assertThat(result).isNotNull();
+        assertThat(result.isDone()).isTrue();
+        assertThat(result.getDescription()).isEqualTo("Put");
+    }
+
+//    @Test
+//    void httpPut_returnsStatusOk(){
+//        //given
+//        int id = repo.save(new Task("foo", LocalDateTime.now())).getId();
+//        // and
+//        String url = "http://localhost:" + port + "/tasks/" + id;
+//        //and
+//        JSONObject taskJsonObject = getJsonObject(true, "Put", LocalDateTime.now());
+//        HttpEntity<String> httpEntity = getHttpEntity(taskJsonObject);
+//        //when
+//        try {
+//            mockMvc.perform(put(url, httpEntity).contentType(MediaType.APPLICATION_JSON))
+//                    .andExpect(status().isOk());
+//        } catch (Exception e) {
+//            throw new RuntimeException(e);
+//        }
+//        restTemplate.put(url,httpEntity);
+//        Task result = repo.findById(id).orElse(null);
+//        //then
+//        assertThat(result).isNotNull();
+//        assertThat(result.isDone()).isTrue();
+//        assertThat(result.getDescription()).isEqualTo("Put");
+//given
+//Task task = new Task("Will you save me?", LocalDateTime.now());
+//
+//    //when
+//    RequestEntity<Task> request = RequestEntity
+//            .post("http://localhost:" + localServerPort + "/tasks/create")
+//            .contentType(MediaType.APPLICATION_JSON)
+//            .body(task);
+//
+//    ResponseEntity<Task> response = testRestTemplate.exchange(request, Task.class);
+//
+//    //then
+//    Task taskBody = response.getBody();
+//
+//    assertTrue(response.getStatusCode().is2xxSuccessful());
+//    assertEquals("Will you save me?", taskBody.getDesc());
+//    }
+
+
+    private static JSONObject getJsonObject(boolean done, String description, LocalDateTime dateTime) {
         var taskJsonObject = new JSONObject();
         try {
-            taskJsonObject.put("done", false);
-            taskJsonObject.put("description", "JsonTest");
-            taskJsonObject.put("deadline", "2023-10-12T23:59:59.999");
+            taskJsonObject.put("done", done);
+            taskJsonObject.put("description", description);
+            taskJsonObject.put("deadline", dateTime);
         } catch (JSONException e) {
             throw new RuntimeException(e);
         }
-        // and
-        HttpEntity<String> httpEntity = new HttpEntity<>(taskJsonObject.toString(), headers);
-        String result = restTemplate.postForObject(url, httpEntity, String.class);
-        try {
-            JsonNode root = objectMapper.readTree(result);
-            //        assertThat(result).isInstanceOf(Task.class);
-//        assertThat(root).flatExtracting("description").isEqualTo("JsonTest");
-        assertThat(result).contains("description");
-            assertThat(result).contains("JsonTest");
-        } catch (JsonProcessingException e) {
-            throw new RuntimeException(e);
-        }
-//        RequestEntity<String> reqBody = RequestEntity.post("http://localhost:" + port + "/tasks").body(reqJson);
-        //when
-//            Task result = restTemplate.postForObject("http://localhost:" + port + "/tasks", reqBody, Task.class);
-        //then
-//        assertThat(result).isInstanceOf(Task.class);
-//        assertThat(result).hasFieldOrProperty("description");
-//        assertThat(result).matches(task-> task.getDescription().equals("foo"));
+        return taskJsonObject;
+    }
+
+    private static HttpEntity<String> getHttpEntity(JSONObject taskJsonObject) {
+        var headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_JSON);
+        return new HttpEntity<>(taskJsonObject.toString(), headers);
     }
 }
